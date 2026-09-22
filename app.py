43150cdf865adb4321e6d3c1f1a1f3d06c87c405
alonsoap19-lab <alonsoap18@gmail.com>
2026-09-22 11:01:30 -0600

    Refresh model cache after zoning update

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.markdown("""<style>
 </style>""", unsafe_allow_html=True)
 
 
-MODEL_CACHE_VERSION = "phase1-zoning-treatment-1"
+MODEL_CACHE_VERSION = "phase1-zoning-treatment-2"
 
 
 @st.cache_resource(show_spinner="Cargando geometria DXF...")
